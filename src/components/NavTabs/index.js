ee0ab80870666efa0b4e3d6ada830139fa361723
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-10-03 19:32:37 -0400

    Create NavTabs and set up links with router

--- src/components/NavTabs/index.js
+++ src/components/NavTabs/index.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function NavTabs() {
-  return <h1>NavTabs</h1>;
+  return (
+    <nav class="navbar navbar-expand-md navbar-dark">
+      <a class="navbar-brand" href="#">
+        Mitchell Underwood
+      </a>
+      <div class="navbar-collapse" id="collapsibleNavbar">
+        <div class="navbar-nav">
+          <div>
+            <Link to="/">About</Link>
+            <Link to="/Portfolio">Portfolio</Link>
+            <Link to="/Contact">Contact</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavTabs;
