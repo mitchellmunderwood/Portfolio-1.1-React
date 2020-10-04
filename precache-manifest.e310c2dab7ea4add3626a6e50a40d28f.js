71f6555461ee0d35e07a96241271be5de04f6ddb
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-10-03 21:50:28 -0400

    Updates

---
+++ precache-manifest.e310c2dab7ea4add3626a6e50a40d28f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1a6a9b4bd3293c95940fa452367b9a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "6fa08ff2206d079b44eb",
+    "url": "/React-Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7e8ea95d8b5e1df8d15e",
+    "url": "/React-Portfolio/static/css/main.f57f0dd8.chunk.css"
+  },
+  {
+    "revision": "6fa08ff2206d079b44eb",
+    "url": "/React-Portfolio/static/js/2.dd28dc59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.dd28dc59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8ea95d8b5e1df8d15e",
+    "url": "/React-Portfolio/static/js/main.3a13784c.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "c1008f16d63d0d26edeb6d7eef1c6722",
+    "url": "/React-Portfolio/static/media/Mitchell_new.c1008f16.jpg"
+  },
+  {
+    "revision": "8b92e6fd68d114bb819686956c040093",
+    "url": "/React-Portfolio/static/media/burger.8b92e6fd.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9db41b572f9a329418bb08d29c91f89b",
+    "url": "/React-Portfolio/static/media/parent_connect.9db41b57.png"
+  },
+  {
+    "revision": "a8231b8c9dcc2db43ce0c4bc5d269c0c",
+    "url": "/React-Portfolio/static/media/password.a8231b8c.png"
+  },
+  {
+    "revision": "58c1a611162025e8b5bfc04343782def",
+    "url": "/React-Portfolio/static/media/ppc.58c1a611.png"
+  },
+  {
+    "revision": "fc726702bec20c49c9d9dba007c2d82c",
+    "url": "/React-Portfolio/static/media/quiz.fc726702.png"
+  },
+  {
+    "revision": "1412afff33d05364252ea7906f7aba18",
+    "url": "/React-Portfolio/static/media/weather.1412afff.png"
+  }
+]);
